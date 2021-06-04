ff43a9db3637c91faf0abfc1d11870c8686bc26b
cheisentrout <cheisentrout@gmail.com>
2021-06-03 21:20:55 -0400

    Installed React app

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import React from 'react'
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Clare's React App!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default App
+
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
