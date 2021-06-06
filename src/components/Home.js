0c2335937a297d9dc3d2f0cd34ac02ceccd4299d
cheisentrout <cheisentrout@gmail.com>
2021-06-06 11:27:30 -0400

    Functional component is now rendering card data

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,85 +7,90 @@ import { Link } from 'react-router-dom'
 import CreateCard from './CreateCard'
 import CreateAccount from './CreateAccount'
 
-// function Home() {
+function Home() {
+
+    const [cards, setCards] = useState([])
+
+    const getCards = () => {
+        axios
+        .get('https://tranquil-wildwood-78396.herokuapp.com/pocket/cards')
+        .then(
+            (response) => {
+                console.log(response.data); // This logs an array of 4 objects
+                setCards(response.data)
+                console.log("Cards array in state: " + cards); // "cards" logs nothing here
+            }
+        )
+    }
+
+    return (
+        <div>
+            <h1>Home (Profile) Component</h1>
+            <Link to="/createaccount">Add User</Link>
+            <Link to="/createcard">Add Card</Link>
+            <section>
+                <button onClick={getCards}>Load Cards</button>
+                {cards.map(card => {
+                    return (
+                        <div className="card" id={card.id}>
+                            <img src={card.card_img} alt={card.title} />
+                            <p>{card.card_text}</p>
+                        </div>
+                    )
+                })}
+            </section>
+        </div>
+    )
+}
+
+/*=============================================================
+=================== CLASS COMPONENT VERSION =================*/
+
+// class Home extends React.Component {
 //
-//     const [cards, setCards] = useState([])
+//     state = {
+//         cards: []
+//     }
 //
-//     const getCards = () => {
+//     getCards = () => {
 //         axios
 //         .get('https://tranquil-wildwood-78396.herokuapp.com/pocket/cards')
 //         .then(
 //             (response) => {
 //                 console.log(response.data);
-//                 setCards(response.data)
-//                 console.log("Cards array in state: " + cards);
+//                 this.setState(
+//                     {
+//                         cards: response.data
+//                     }
+//                 )
 //             }
 //         )
 //     }
 //
-//     return (
-//         <div>
-//             <h1>Home (Profile) Component</h1>
-//             <Link to="/createaccount">Add User</Link>
-//             <Link to="/createcard">Add Card</Link>
-//             <section>
-//                 <button onClick={getCards}>Load Cards</button>
-//                 {cards.map(card => {
-//                     <div className="card">
-//                         <img src={card.card_img} alt={card.title} />
-//                         <p>{card.card_text}</p>
-//                     </div>
-//                 })}
-//             </section>
-//         </div>
-//     )
+//     componentDidMount = () => {
+//         this.getCards()
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Home (Profile) Component</h1>
+//                 <Link to="/createaccount">Add User</Link>
+//                 <Link to="/createcard">Add Card</Link>
+//                 <section>
+//                     <button onClick={this.getCards}>Load Cards</button>
+//                     {this.state.cards.map(card => {
+//                         return (
+//                             <div className="card" id={card.id}>
+//                                 <img src={card.card_img} alt={card.title} />
+//                                 <p>{card.card_text}</p>
+//                             </div>
+//                         )
+//                     })}
+//                 </section>
+//             </div>
+//         )
+//     }
 // }
-
-class Home extends React.Component {
-
-    state = {
-        cards: []
-    }
-
-    getCards = () => {
-        axios
-        .get('https://tranquil-wildwood-78396.herokuapp.com/pocket/cards')
-        .then(
-            (response) => {
-                console.log(response.data);
-                this.setState(
-                    {
-                        cards: response.data
-                    }
-                )
-            }
-        )
-    }
-
-    componentDidMount = () => {
-        this.getCards()
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Home (Profile) Component</h1>
-                <Link to="/createaccount">Add User</Link>
-                <Link to="/createcard">Add Card</Link>
-                <section>
-                    <button onClick={this.getCards}>Load Cards</button>
-                    {this.state.cards.map(card => {
-                        return (
-                            <div className="card" id={card.id}>
-                                <img src={card.card_img} alt={card.title} />
-                                <p>{card.card_text}</p>
-                            </div>
-                        )
-                    })}
-                </section>
-            </div>
-        )
-    }
-}
 
 export default Home
